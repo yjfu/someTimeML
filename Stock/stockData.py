d2f5442990c3878dce6f7eee378712bb83989444
yjfu <yjfu_0707@qq.com>
2016-07-19 10:02:30 +0800

    wrong

---
+++ Stock/stockData.py
@@ -0,0 +1,11 @@
+import tushare as ts
+
+def download(sockNum):
+
+    d = ts.get_hist_data(sockNum)
+    d.to_csv(sockNum+".data",sep = "\t")
+    out = open(sockNum,"w")
+    out.write(d.__str__())
+    out.close()
+
+
